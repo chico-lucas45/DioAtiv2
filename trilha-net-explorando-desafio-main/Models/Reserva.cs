8a4fdd8f0325611d1912046da180b8a0ea84dc99
chico-lucas45 <85258610+chico-lucas45@users.noreply.github.com>
2023-04-20 14:03:15 -0300

    Add files via upload

---
+++ trilha-net-explorando-desafio-main/Models/Reserva.cs
@@ -0,0 +1,52 @@
+namespace DesafioProjetoHospedagem.Models
+{
+    public class Reserva
+    {
+        public List<Pessoa> Hospedes { get; set; }
+        public Suite Suite { get; set; }
+        public int DiasReservados { get; set; }
+
+        public Reserva() { }
+
+        public Reserva(int diasReservados)
+        {
+            DiasReservados = diasReservados;
+        }
+
+        public void CadastrarHospedes(List<Pessoa> hospedes)
+        {
+            if (hospedes.Count <= Suite.Capacidade)
+            {
+                Hospedes = hospedes;
+            }
+            else
+            {
+                throw new Exception();
+            }
+        }
+
+        public void CadastrarSuite(Suite suite)
+        {
+            Suite = suite;
+        }
+
+        public int ObterQuantidadeHospedes()
+        {   
+            return Hospedes.Count;
+        }
+
+        public decimal CalcularValorDiaria()
+        {
+
+            decimal valor = DiasReservados * Suite.ValorDiaria;
+
+
+            if (DiasReservados >= 10)
+            {
+                valor -= (valor * (10m / 100m));
+            }
+
+            return valor;
+        }
+    }
+}
